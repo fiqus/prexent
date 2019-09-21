e1986efd422bca1472259f59393a39eb04f54fb4
mlambir <mlambir@gmail.com>
2019-09-21 01:10:56 -0300

    display slides and animate with css

---
+++ lib/prexent_web/live/slides.ex
@@ -0,0 +1,27 @@
+defmodule PrexentWeb.SlidesLive do
+  @moduledoc false
+  use Phoenix.LiveView
+  use Phoenix.HTML
+
+  @left_key 38
+
+  def render(assigns) do
+    Phoenix.View.render(PrexentWeb.PageView, "slides.html", assigns)
+  end
+
+  def mount(_ , socket) do
+    {:ok, assign(socket, slides: ["test slide 1", "test slide 2", "test slide 3"], slide: 1)}
+  end
+
+  def handle_event("keyup", %{"key" => "ArrowLeft"}, socket) do
+    {:noreply, assign(socket, :slide, max(socket.assigns.slide - 1, 0))}
+  end
+
+  def handle_event("keyup", %{"key" => "ArrowRight"}, socket) do
+    {:noreply, assign(socket, :slide, min(socket.assigns.slide + 1, length(socket.assigns.slides) - 1))}
+  end
+
+  def handle_event("keyup", _, socket) do
+    {:noreply, socket}
+  end
+end
