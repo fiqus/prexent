4368743b4515032b2a63f654fdc67734a54e00b9
Pablo Brudnick <pbrudnick@gmail.com>
2019-09-22 03:59:08 -0300

    installer published in hex

--- installer/mix.exs
+++ installer/mix.exs
@@ -1,13 +1,31 @@
 defmodule Prexent.New.MixProject do
   use Mix.Project
 
+  @version "0.1.0"
+  @github_path "spawnfest/prexent"
+  @url "https://github.com/#{@github_path}"
+
   def project do
     [
       app: :prexent_new,
       version: "0.1.0",
-      elixir: "~> 1.9",
+      elixir: "~> 1.5",
       start_permanent: Mix.env() == :prod,
-      deps: deps()
+      deps: deps(),
+      package: [
+        maintainers: [
+          "Pablo Brudnick",
+          "Diego Calero",
+          "Mariano Lambir",
+          "Joaquín Mansilla"
+        ],
+        licenses: ["MIT"],
+        links: %{github: @url},
+        files: ~w(lib mix.exs README.md)
+      ],
+      source_url: @url,
+      docs: docs(),
+      description: "Prexent installer"
     ]
   end
 
@@ -21,8 +39,13 @@ defmodule Prexent.New.MixProject do
   # Run "mix help deps" to learn about dependencies.
   defp deps do
     [
-      # {:dep_from_hexpm, "~> 0.3.0"},
-      # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"}
+      {:ex_doc, "~> 0.19", only: :dev, runtime: false}
+    ]
+  end
+
+  defp docs do
+    [
+      source_url_pattern: "https://github.com/#{@github_path}/blob/v#{@version}/installer/%{path}#L%{line}"
     ]
   end
 end
